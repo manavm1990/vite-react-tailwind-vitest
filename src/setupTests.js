e6c1c31feae0531002e3845b2daa5dedf9f09384
Manav Misra <manavm@visionwebsoft.com>
2022-10-24 05:18:39 -0500

    Setup tests with cleanup

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,14 @@
+/**
+ * Some examples online show that we need to extend Vitest's 'expect' with 'jest-dom':
+ * expect.extend
+ *
+ * However, this is not necessary. Vitest's 'expect' already has all the 'jest-dom' matchers.
+ * Just import it here as part of test setup.
+ */
 import "@testing-library/jest-dom";
+import { cleanup } from "@testing-library/react";
+import { afterEach } from "vitest";
+
+afterEach(() => {
+  cleanup();
+});
