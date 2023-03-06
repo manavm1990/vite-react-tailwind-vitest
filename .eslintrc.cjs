30079385c73831677add3de504dd336f83d702b8
Manav Misra <manavm@visionwebsoft.com>
2023-03-06 09:32:09 -0600

    'plugin:testing-library/react'

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,7 @@ module.exports = {
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
     "plugin:jsx-a11y/recommended",
+    'plugin:testing-library/react',
     "standard",
     "prettier",
   ],
