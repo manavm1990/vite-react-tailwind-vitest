bf7d7cffba9e2a660481c9e4a4151ecd36cf1d79
Manav Misra <manavm@visionwebsoft.com>
2023-07-05 07:50:41 -0500

    chore:

--- src/tests/app.test.jsx
+++ src/tests/app.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import App from "@/App";
 import { expect, test } from "vitest";
 
 test("renders correctly", () => {
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,6 +17,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./src/setupTests.js",
+    setupFiles: "./src/tests/setup-tests.js",
   },
 });
